9b0022fb8c3c9a036d609ba2231953f02fbd9730
Weam Adel <weamadel1994@gmail.com>
2024-01-23 00:41:58 +0200

    register the service worker

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
 import './styles/index.css';
+
+if ('serviceWorker' in navigator) {
+	try {
+		const registration = await navigator.serviceWorker.register('../service-worker.js', {
+			scope: '/',
+		});
+		console.log('SW registered: ', registration);
+	} catch (error) {
+		console.log('SW registration failed');
+	}
+}
